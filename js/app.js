f5996419a372532d9f0f5ec8c3d5e4229888764e
PopovaOlha <fc.odessa.10@gmail.com>
2023-04-25 20:35:40 +0300

    feat: add new object keyboard with a properties and methods, create a dom elements in a js file. wtite style for these elements in style.css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,73 @@
 "use strict"
+
+const Keyboard = {
+    elements: {
+        container: null,
+        textArea: null,
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventsHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: "",
+        capsLock: false
+    },
+
+    init({elements} = this) {
+       let {container, textArea, main, keysContainer} = elements;
+
+        // create elements
+       container = document.createElement("div");
+       textArea = document.createElement("textarea");
+       main = document.createElement("div");
+       keysContainer = document.createElement("div");
+
+       //setup elements
+       container.classList.add("container");
+       textArea.classList.add("body--textarea");
+       main.classList.add("keyboard");
+       keysContainer.classList.add("keyboard__keys");
+
+       //add to DOM
+       container.append(textArea);
+       main.append(keysContainer);
+       document.body.append(container, main);
+
+    },
+
+    _createKeys() {
+
+    },
+
+    _triggerEvent(handlerName) {
+      console.log("Event Triggered! Event name" + handlerName);
+    },
+
+    _toggleCapsLock() {
+        console.log("capslock Toggled!")
+    },
+
+    _changeLanguage() {
+
+    },
+
+    keyboardSynchronization() {
+
+    }
+};
+
+document.addEventListener('keydown', function(event) {
+    if (event.key) {
+    }
+});
+
+  window.addEventListener("DOMContentLoaded", startInit);
+  function startInit() {
+    Keyboard.init();
+  }
