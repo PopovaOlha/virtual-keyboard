a13c568b372c1c91f22c3d5c74cf92072aabd2e0
PopovaOlha <fc.odessa.10@gmail.com>
2023-04-26 23:27:29 +0300

    feat: create a keys for keyboard

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import keyLayout from './keys.js';
+
 const Keyboard = {
   elements: {
     container: null,
@@ -33,6 +35,7 @@ const Keyboard = {
     textArea.classList.add('body--textarea');
     main.classList.add('keyboard');
     keysContainer.classList.add('keyboard__keys');
+    keysContainer.appendChild(this._createKeys());
 
     // add to DOM
     container.append(textArea);
@@ -40,28 +43,120 @@ const Keyboard = {
     document.body.append(container, main);
   },
 
-  createKeys() {},
+  _createKeys({properties} = this) {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+    'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\'', 'Del',
+    'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
+    'ShiftL', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'ShiftR',
+    'CtrlL', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'CtrlR']; 
 
-  triggerEvent(handlerName) {
+    keyLayout.forEach(key => {
+    const keyElement = document.createElement('button');
+    const insertLineBreak = ['Backspace', 'Del', 'Enter', 'ShiftR', 'CtrlR' ].indexOf(key) !== -1;
+    
+    //add attributes of a key
+    keyElement.setAttribute('type', 'button');
+    keyElement.classList.add('keyboard__key');
+
+    switch (key) {
+        case 'Backspace':
+            keyElement.classList.add('specific-color', 'key--max-wide');
+            keyElement.innerHTML = 'Backspace';
+
+            keyElement.addEventListener('click', () => {
+                properties.value = properties.value.substring(0, properties.value.length -1);
+                this._triggerEvent('oninput');
+            })
+
+            break;
+
+        case 'CapsLock':
+            keyElement.classList.add('specific-color', 'key--max-wide'); 
+            keyElement.innerHTML = 'CapsLock';
+
+            keyElement.addEventListener('click', () => {
+                this._toggleCapsLock();
+            })
+
+            break;
+
+        case 'Enter':
+            keyElement.classList.add('specific-color', 'key--wide');
+            keyElement.innerHTML = 'Enter';  
+            
+            keyElement.addEventListener('click', () => {
+                properties.value += '\n';
+                this._triggerEvent('oninput');
+            })
+
+            break;
+
+        case 'Space':
+            keyElement.classList.add('specific-color', 'key--extra-wide');
+            keyElement.innerHtml = " ";  
+
+            keyElement.addEventListener('click', () => {
+                properties.value += " ";
+                this._triggerEvent('oninput');
+            })
+
+            break;
+
+        case 'ShiftL':
+            keyElement.classList.add('specific-color', 'key--max-wide');
+            keyElement.innerHTML = 'Shift';
+            
+            keyElement.addEventListener('click', () => {
+                alert('поменять символы');
+            })
+
+            break;
+
+        default:
+            keyElement.textContent = key.toLowerCase(); 
+            
+            keyElement.addEventListener('click', () => {
+                properties.value = properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+                this._triggerEvent('oninput');
+            })
+
+            break;
+    }
+
+    fragment.appendChild(keyElement);
+
+    if (insertLineBreak) {
+        fragment.appendChild(document.createElement('br'));
+    }
+    
+   })
+    return fragment;
+  },
+
+  _triggerEvent(handlerName) {
     console.log(`Event Triggered! Event name${handlerName}`);
   },
 
-  toggleCapsLock() {
+  _toggleCapsLock() {
     console.log('capslock Toggled!');
   },
 
   changeLanguage() {},
 
   keyboardSynchronization() {
-
+   
   },
 };
 
 document.addEventListener('keydown', (event) => {
-  if (event.key) {
-    console.log(event.key);
-  }
-});
+    if (event.key) {
+      console.log(event.key)
+    }
+  })
+
+
+ 
 
 function startInit() {
   Keyboard.init();
