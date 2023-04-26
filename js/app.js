8799e219251b35ab8a8dcaa31423213c0c3ac748
PopovaOlha <fc.odessa.10@gmail.com>
2023-04-26 17:31:05 +0300

    feat: add eslint

--- js/app.js
+++ js/app.js
@@ -1,73 +1,70 @@
-"use strict"
-
 const Keyboard = {
-    elements: {
-        container: null,
-        textArea: null,
-        main: null,
-        keysContainer: null,
-        keys: []
-    },
+  elements: {
+    container: null,
+    textArea: null,
+    main: null,
+    keysContainer: null,
+    keys: [],
+  },
 
-    eventsHandlers: {
-        oninput: null,
-        onclose: null
-    },
+  eventsHandlers: {
+    oninput: null,
+    onclose: null,
+  },
 
-    properties: {
-        value: "",
-        capsLock: false
-    },
+  properties: {
+    value: '',
+    capsLock: false,
+  },
 
-    init({elements} = this) {
-       let {container, textArea, main, keysContainer} = elements;
+  init({ elements } = this) {
+    let {
+      container, textArea, main, keysContainer,
+    } = elements;
 
-        // create elements
-       container = document.createElement("div");
-       textArea = document.createElement("textarea");
-       main = document.createElement("div");
-       keysContainer = document.createElement("div");
+    // create elements
+    container = document.createElement('div');
+    textArea = document.createElement('textarea');
+    main = document.createElement('div');
+    keysContainer = document.createElement('div');
 
-       //setup elements
-       container.classList.add("container");
-       textArea.classList.add("body--textarea");
-       main.classList.add("keyboard");
-       keysContainer.classList.add("keyboard__keys");
+    // setup elements
+    container.classList.add('container');
+    textArea.classList.add('body--textarea');
+    main.classList.add('keyboard');
+    keysContainer.classList.add('keyboard__keys');
 
-       //add to DOM
-       container.append(textArea);
-       main.append(keysContainer);
-       document.body.append(container, main);
+    // add to DOM
+    container.append(textArea);
+    main.append(keysContainer);
+    document.body.append(container, main);
+  },
 
-    },
+  createKeys() {},
 
-    _createKeys() {
+  triggerEvent(handlerName) {
+    console.log(`Event Triggered! Event name${handlerName}`);
+  },
 
-    },
+  toggleCapsLock() {
+    console.log('capslock Toggled!');
+  },
 
-    _triggerEvent(handlerName) {
-      console.log("Event Triggered! Event name" + handlerName);
-    },
+  changeLanguage() {},
 
-    _toggleCapsLock() {
-        console.log("capslock Toggled!")
-    },
+  keyboardSynchronization() {
 
-    _changeLanguage() {
-
-    },
-
-    keyboardSynchronization() {
-
-    }
+  },
 };
 
-document.addEventListener('keydown', function(event) {
-    if (event.key) {
-    }
+document.addEventListener('keydown', (event) => {
+  if (event.key) {
+    console.log(event.key);
+  }
 });
 
-  window.addEventListener("DOMContentLoaded", startInit);
-  function startInit() {
-    Keyboard.init();
-  }
+function startInit() {
+  Keyboard.init();
+}
+
+window.addEventListener('DOMContentLoaded', startInit);
